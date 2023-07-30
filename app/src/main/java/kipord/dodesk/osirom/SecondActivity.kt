c9b4bfbc15e7a920897a9a1592c6e06772050299
silneevdamir2 <silneevdamir2@outlook.com>
2023-07-30 06:14:57 +0300

    Initial commit

---
+++ app/src/main/java/kipord/dodesk/osirom/SecondActivity.kt
@@ -0,0 +1,30 @@
+package kipord.dodesk.osirom
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.dodesk.osirom.R
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w: Window = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+        val btn = findViewById<View>(R.id.btn)
+
+        btn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/kipord/dodesk/osirom/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package kipord.dodesk.osirom
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.dodesk.osirom.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w: Window = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, SecondActivity::class.java))
+            finish()
+        }, 4000)
+    }
+}
